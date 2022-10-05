ab79d8106fa89f1519c72b5970dc583364035e9b
EunHee Ko <faithee2019@gmail.com>
2022-10-05 22:19:37 +0900

    Average
    Average

--- src/programmers/level1/Average.java
+++ src/programmers/level1/Average.java
@@ -1,5 +1,13 @@
 package programmers.level1;
 
 public class Average {
+	//합을 구하고
+    for(int i =0 ; i < arr.length ; i++){
+        answer += arr[i];
+    }
+    //개수로 나누기
+    answer = answer/arr.length;
 
+
+    return answer;
 }
