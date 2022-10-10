2758eae069deda0815d03c949e5f17fcd3dc22c4
EunHee Ko <faithee2019@gmail.com>
2022-10-10 22:30:22 +0900

    hashard
    hashard

--- src/programmers/level1/Hashard.java
+++ src/programmers/level1/Hashard.java
@@ -4,25 +4,38 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Hashard {
-	
-	//자리수 숫자 합 구하는 함수
-    public int intSum(int num) {
+
+	// 자리수 숫자 합 구하는 함수
+	public int intSum(int num) {
 		int[] digits = Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
-	    int result = Arrays.stream(digits).sum();
-        return result;
-    }
+		int result = Arrays.stream(digits).sum();
+		return result;
+	}
+
+	// 새로운 방식
+	public boolean newIntSum(int num) {
+		String[] temp = String.valueOf(num).split("");
+
+		int sum = 0;
+		for (String s : temp) {
+			sum += Integer.parseInt(s);
+		}
+
+		if (num % sum == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Hashard sum = new Hashard();
 		int x = 18;
-		 boolean answer = false;
-	        if(x % sum.intSum(x) == 0){
-	            answer = true;
-	        }
-	        
-		
+		boolean answer = false;
+		if (x % sum.intSum(x) == 0) {
+			answer = true;
+		}
+		System.out.println("result: "+sum.newIntSum(x));
 	}
-
-
 }
