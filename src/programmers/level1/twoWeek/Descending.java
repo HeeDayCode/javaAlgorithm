e23f0c866c712cf711e22723196af9876c564d31
EunHee Ko <faithee2019@gmail.com>
2022-10-16 16:07:16 +0900

    descending
    descending

--- src/programmers/level1/twoWeek/Descending.java
+++ src/programmers/level1/twoWeek/Descending.java
@@ -1,6 +1,6 @@
 package programmers.level1.twoWeek;
 
-public class Descending {
+public class Descending { //정수 내림차순으로 배치하기 문제풀이
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
