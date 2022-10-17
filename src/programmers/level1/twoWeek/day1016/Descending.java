769772c34c7911e812ac7818452a33f2cbf4bbac
EunHee Ko <faithee2019@gmail.com>
2022-10-17 21:30:13 +0900

    descending
    descending

---
+++ src/programmers/level1/twoWeek/day1016/Descending.java
@@ -0,0 +1,28 @@
+package programmers.level1.twoWeek.day1016;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Descending { // 정수 내림차순으로 배치하기 문제풀이
+
+	public static void main(String[] args) {
+		long answer = 0;
+		long n = 2343525;
+
+		String[] temp = String.valueOf(n).split("");
+
+		int[] values = Arrays.stream(temp).mapToInt(Integer::parseInt).toArray();
+
+		Integer[] descendingValues = Arrays.stream(values).boxed().toArray(Integer[]::new);
+		Arrays.sort(descendingValues, Collections.reverseOrder());
+
+		// System.out.println(Arrays.toString(descendingValues));
+
+		String result = "";
+		for (int i = 0; i < descendingValues.length; i++) {
+			result += descendingValues[i];
+		}
+
+	}
+
+}
