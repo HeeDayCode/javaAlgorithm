8cfdbaf789652e52eae41a978082bb92c9517715
EunHee Ko <faithee2019@gmail.com>
2022-10-19 23:26:37 +0900

    ReverseInt
    ReverseInt

---
+++ src/programmers/level1/twoWeek/day1016/ReverseInt.java
@@ -0,0 +1,16 @@
+package programmers.level1.twoWeek.day1016;
+
+public class ReverseInt {
+    String res = "";
+    public int reverseInt(int n){
+        res = "";
+        Integer.toString(n).chars().sorted().forEach(c -> res = Character.valueOf((char)c) + res);
+        return Integer.parseInt(res);
+    }
+
+    // 아래는 테스트로 출력해 보기 위한 코드입니다.
+    public static void  main(String[] args){
+        ReverseInt ri = new ReverseInt();
+        System.out.println(ri.reverseInt(118372));
+    }
+}
