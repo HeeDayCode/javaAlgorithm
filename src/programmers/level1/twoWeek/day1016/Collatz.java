f7e61fe71b3f95016c2526795e7cea28bce04344
EunHee Ko <faithee2019@gmail.com>
2022-10-22 18:29:06 +0900

    update Collatz
    update Collatz

--- src/programmers/level1/twoWeek/day1016/Collatz.java
+++ src/programmers/level1/twoWeek/day1016/Collatz.java
@@ -4,7 +4,7 @@ public class Collatz {
 
 	public static void main(String[] args) {
        
-        long number = num;
+        long number = 123;
         int answer = 0;
         for(answer = 0; answer < 500; answer++){
             if(number == 1) {} //return answer;
@@ -13,9 +13,8 @@ public class Collatz {
                 else number = number * 3 + 1;
             }
         } 
-        //return -1;
-        
-        //System.out.println(resultCount);
+
+        System.out.println(answer);
 
 	}
 
