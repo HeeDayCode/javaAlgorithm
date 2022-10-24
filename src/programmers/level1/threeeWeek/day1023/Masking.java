87a3495c80f466ba27bc7831b56b24a82be62d97
EunHee Ko <faithee2019@gmail.com>
2022-10-24 23:25:28 +0900

    Masking
    Masking

---
+++ src/programmers/level1/threeeWeek/day1023/Masking.java
@@ -0,0 +1,20 @@
+package programmers.level1.threeeWeek.day1023;
+
+public class Masking {
+
+	public static void main(String[] args) {
+		String answer = "";
+		String phone_number = "010222222";
+        String[] temp = String.valueOf(phone_number).split("");
+         
+        for(int i = 0; i<temp.length -4 ;i++){
+            temp[i] = "*";
+        }
+        
+        for (String s : temp) {
+			answer +=   s;
+		}
+        System.out.println(answer);
+	}
+
+}
