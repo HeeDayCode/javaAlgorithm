60f1401bc353abc63ea0389f1e74a8d8e84181ee
EunHee Ko <faithee2019@gmail.com>
2022-10-24 23:24:54 +0900

    ArrayPush
    ArrayPush

---
+++ src/programmers/level1/threeeWeek/day1023/ArrayPush.java
@@ -0,0 +1,28 @@
+package programmers.level1.threeeWeek.day1023;
+
+import java.util.Arrays;
+
+public class ArrayPush {
+
+	public static void main(String[] args) {
+
+		int cnt=0;
+		int[] arr=	{5, 9, 7, 10};	
+		int divisor=	5;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]%divisor==0) cnt++;
+        }
+
+        if(cnt==0){
+            int[] answer={-1};
+            System.out.println(answer);
+        }
+        int[] answer=new int[cnt];
+        for(int i=0,j=0; i<arr.length; i++){
+            if(arr[i]%divisor==0) answer[j++]=arr[i];
+        }
+
+        Arrays.sort(answer);
+        System.out.println(answer);
+	}
+}
