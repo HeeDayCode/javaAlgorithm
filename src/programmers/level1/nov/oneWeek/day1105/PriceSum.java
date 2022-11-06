3eedf3266bcafe34bb62daf5750b445f81baec5a
EunHee Ko <faithee2019@gmail.com>
2022-11-06 11:33:28 +0900

    PriceSum
    PriceSum

---
+++ src/programmers/level1/nov/oneWeek/day1105/PriceSum.java
@@ -0,0 +1,20 @@
+package programmers.level1.nov.oneWeek.day1105;
+
+public class PriceSum {
+
+	public static void main(String[] args) {
+		int count = 6;
+		int price = 300;
+		int money = 500;
+		
+		 long temp = 0;
+	        for(int i = 1; i <= count; i++)
+	        {
+	            temp += i * price;            
+	        }
+
+	        if (temp > money)
+	        	System.out.println(temp-money);
+	        else System.out.println(0);
+	}
+}
