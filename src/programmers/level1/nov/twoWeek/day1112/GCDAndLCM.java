feec90a02075494223d0aa766d794eee114cae31
EunHee Ko <faithee2019@gmail.com>
2022-11-12 19:16:16 +0900

    GCDAndLCM
    GCDAndLCM

---
+++ src/programmers/level1/nov/twoWeek/day1112/GCDAndLCM.java
@@ -0,0 +1,29 @@
+package programmers.level1.nov.twoWeek.day1112;
+
+public class GCDAndLCM {
+
+	public static void main(String[] args) {
+		int[] answer = new int[2];
+		int num1 = 5;
+		int num2 = 10;
+		
+        int max = 0; //최대 공약수
+
+        for(int i=1; i<=num1 && i<=num2; i++) 
+        {
+            if(num1%i==0 && num2%i==0)
+            {
+                max = i;
+            }
+        }       
+
+        int min = (num1*num2)/max; //최소 공배수
+
+        answer[0] = max;
+        answer[1] = min;
+
+       System.out.println(answer[0]+" / "+answer[1]);
+
+	}
+
+}
