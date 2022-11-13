2600a11d487092818f00db6f4ae873ba0c994ee8
EunHee Ko <faithee2019@gmail.com>
2022-11-13 22:13:16 +0900

    sameNumberDelete
    sameNumberDelete

--- src/programmers/level1/nov/twoWeek/day1112/sameNumberDelete.java
+++ src/programmers/level1/nov/twoWeek/day1112/sameNumberDelete.java
@@ -6,39 +6,32 @@ import java.util.List;
 
 public class sameNumberDelete {
 	
-	public int[] deleteIndex(int []arr, int deleteNum) {
-		int[] result = new int[arr.length];
-
-		int[] ints = {1, 2, 3};
-		List<Integer> intList = new ArrayList<Integer>(result.length);
-		for (int i =0; i < result.length ; i++)
-		{
-		    intList.add(i);
-		}
-		
-		intList.remove(deleteNum);
-		
-		int[] arr1 = new int[intList.size()];
-	    for (int i = 0 ; i < intList.size() ; i++) {
-	    	result[i] = intList.get(i).intValue();
-	    }
-		 
-		return result;
-	}
+	public int[] solution(int []arr) {
+        int[] answer = {};
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        
+        int num = -1; //원소가 0~9까지 이므로 영향 안받는 아무 숫자.
+        for(int i=0; i<arr.length; i++){
+             if(arr[i] != num){
+                 arrList.add(arr[i]);
+                 num = arr[i];
+             }
+        }
+        
+        answer = new int[arrList.size()]; //answer 리스트 크기 선언.
+        for(int i=0; i<answer.length; i++){
+            answer[i] = arrList.get(i); 
+        }
+        
+        return answer;
+    }
 	
 	
 	public static void main(String[] args) {
-		int []arr = {1,1,2,3,4,5};
+		int []arr = {1,1,2,3,4,5,5,6,7};
 		sameNumberDelete result = new sameNumberDelete();
-		System.out.println(Arrays.toString(result.deleteIndex(arr, 2)));
+		System.out.println(Arrays.toString(result.solution(arr)));
 		
 	}
-	
-	
-	
-	
-	
-	
-	
 
 }
