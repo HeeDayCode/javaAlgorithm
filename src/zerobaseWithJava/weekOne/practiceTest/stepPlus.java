b16113b9d83c0db9db1ce6fa0899e2bc6a4e9684
EunHee Ko <faithee2019@gmail.com>
2022-12-15 23:08:02 +0900

    stepPlus
    stepPlus

---
+++ src/zerobaseWithJava/weekOne/practiceTest/stepPlus.java
@@ -0,0 +1,22 @@
+package zerobaseWithJava.weekOne.practiceTest;
+
+public class stepPlus {
+
+	
+	public static int fact(int n) {
+
+		if (n <= 1)
+			return n;
+
+		else 
+			return fact(n-1) + n;
+
+	}
+
+	public static void main(String[] args) {
+		int n = 10;
+		int answer = fact(n);
+        System.out.println(answer);
+	}
+
+}
