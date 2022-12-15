76be9941fb65987a98447fdebca25a5788561601
EunHee Ko <faithee2019@gmail.com>
2022-12-15 23:52:47 +0900

    Stringcheck
    Stringcheck

---
+++ src/zerobaseWithJava/weekOne/practiceTest/Stringcheck.java
@@ -0,0 +1,28 @@
+package zerobaseWithJava.weekOne.practiceTest;
+
+import java.util.Arrays;
+
+public class Stringcheck {
+
+	public static void main(String[] args) {
+		int answer = 0;
+        int max = 0;
+        String s = "123927821";
+        int[] count = new int[10];
+
+        for(int i=0; i<s.length(); i++){
+            for(int j=0; j<10; j++){
+                if( s.charAt(i) == Character.forDigit(j, 10) ) count[j]++;
+            }
+        }
+        for(int i=0; i<10; i++){
+            if(count[i]>max){
+                max = count[i];
+                answer = i;
+            }
+        }
+        System.out.println(answer);
+
+	}
+
+}
