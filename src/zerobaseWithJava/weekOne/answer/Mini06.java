5ca3b15814337d2fba204c854c49631a541ec423
EunHee Ko <faithee2019@gmail.com>
2022-12-20 00:53:10 +0900

    Answer Reference
    Answer Reference

---
+++ src/zerobaseWithJava/weekOne/answer/Mini06.java
@@ -0,0 +1,44 @@
+package zerobaseWithJava.weekOne.answer;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.concurrent.CancellationException;
+
+public class Mini06 {
+
+
+	public static void main(String[] args) {
+
+		List<candidate> 후보자들 = new ArrayList<>();
+		후보자들.add(new candidate(1, "이재명"));
+		후보자들.add(new candidate(2, "윤석렬"));
+		후보자들.add(new candidate(3, "심상정"));
+		후보자들.add(new candidate(4, "안철수"));
+
+		int voteCount = 100000;
+
+		int min = 0;
+		int max = 3;
+		Random random = new Random();
+
+		int i = 0;
+		for (i = 1; i <= voteCount; i++) {
+			int vote = random.nextInt(max - min + 1) + min;
+
+			후보자들.get(vote).addVote();
+
+			System.out.println("");
+			System.out.println(후보자들.get(vote).getName());
+
+			for (candidate x : 후보자들) {
+				x.setTotalVoteCount(i);
+				String msg = x.getDisplayInfo();
+				System.out.println(msg);
+			}
+		}
+
+
+
+	}
+
+}
