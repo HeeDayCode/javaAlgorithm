e71ddcbf65e14324d66cf9d7ff9903a52d901753
EunHee Ko <faithee2019@gmail.com>
2022-12-22 21:44:53 +0900

    shiftMove
    shiftMove

--- src/zerobaseWithJava/weekTwo/pratice5Set/twoSet/shiftMove3.java
+++ src/zerobaseWithJava/weekTwo/pratice5Set/twoSet/shiftMove3.java
@@ -8,8 +8,10 @@ public class shiftMove3 {
 	        int[] data = {1,2,3,4,5};
 
 	       // int tmp = data[data.length-1]; // 배열의 맨 마지막 값
-	        for(int i=data.length-2; i>=0; i--) // 배열의 맨 마지막 값을 제외한 마지막 값
-	            data[i+1] = data[i]; // 한 칸씩 뒤로 이동
+	        for(int i=data.length-2; i>=0; i--) {
+	        	 data[i+1] = data[i]; 
+	        } // 배열의 맨 마지막 값을 제외한 마지막 값
+	           // 한 칸씩 뒤로 이동
 	       // data[0] = tmp;
 	        
 	        for(int i=0; i<data.length; i++)
