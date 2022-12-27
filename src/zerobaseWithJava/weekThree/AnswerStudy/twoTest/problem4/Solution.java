dfcc332ab9019d47c2bafaaa37aff43c0a79d756
EunHee Ko <faithee2019@gmail.com>
2022-12-27 23:42:00 +0900

    currentQueue
    currentQueue

---
+++ src/zerobaseWithJava/weekThree/AnswerStudy/twoTest/problem4/Solution.java
@@ -0,0 +1,20 @@
+package zerobaseWithJava.weekThree.AnswerStudy.twoTest.problem4;
+class Solution {
+    public int solution (int delay, int capacity, int[] times) {
+        int currentTime = 0;
+        int currentQueue = 0;
+        int result = 0;
+        for (int time: times) {
+            currentTime += time;
+            int sent = currentTime / delay;
+            currentQueue = Math.max(0, currentQueue - sent);
+            if (currentQueue == capacity) {
+                result++;
+            } else {
+                currentQueue++;
+            }
+            currentTime %= delay;
+        }
+        return result;
+    }
+}
