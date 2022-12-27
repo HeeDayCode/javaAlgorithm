3c884656788b464c8575ef500ddba358ab7b4ecb
EunHee Ko <faithee2019@gmail.com>
2022-12-27 23:07:49 +0900

    swap
    swap

---
+++ src/zerobaseWithJava/weekThree/AnswerStudy/oneTest/problem3/Solution.java
@@ -0,0 +1,21 @@
+package zerobaseWithJava.weekThree.AnswerStudy.oneTest.problem3;
+
+
+class Solution {
+
+    public int solution (int N) {
+        int a = 1, b = 2;
+
+        if (N == 1) {
+            return a;
+        }
+
+        for (int i = 3; i <= N; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        return b;
+    }
+}
