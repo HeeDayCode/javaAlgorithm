3146d9d213a5c6dd8210c6cd7086023ddf9baa81
EunHee Ko <faithee2019@gmail.com>
2022-12-27 23:41:09 +0900

    sort
    sort

---
+++ src/zerobaseWithJava/weekThree/AnswerStudy/twoTest/problem1/Solution.java
@@ -0,0 +1,16 @@
+package zerobaseWithJava.weekThree.AnswerStudy.twoTest.problem1;
+
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] + 1 != numbers[i + 1]) {
+                return numbers[i] + 1;
+            }
+        }
+        return -1;
+    }
+}
