acea7e398068b14860f0e1372cdd94210ddef146
EunHee Ko <faithee2019@gmail.com>
2023-01-07 22:45:15 +0900

    numchange
    numchange

--- src/test20230105/num1.java
+++ src/test20230105/num1.java
@@ -4,7 +4,9 @@ public class num1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for (int i = 0; i < args.length; i++) {
+			System.out.println();
+		}
 	}
 
 }
